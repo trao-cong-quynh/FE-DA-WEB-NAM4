6ca8508c7f69c4960f28fffef480695fe0c9dd5f
os1 <os1@gmail.com>
2025-04-09 21:13:15 +0700

    Fix import path for Navbar component 2

--- src/pages/Confirmation.jsx
+++ src/pages/Confirmation.jsx
@@ -97,13 +97,16 @@ const Confirmation = () => {
     };
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/api/create-payment", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(paymentData),
-      });
+      const response = await fetch(
+        "https://be-da-web-nam4.onrender.com/api/create-payment",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(paymentData),
+        }
+      );
 
       const data = await response.json();
       if (data.error) {
